e1c753b74d03f124680cc3ea99b6995f5932769f
HectorLMF <alu0100595604@ull.edu.es>
2025-09-29 15:27:30 +0100

    Final de tutorizada

---
+++ sum4_v2.v
@@ -0,0 +1,4 @@
+//implementación alternativa que no utilice los “Full-Adder” de 1 bit, sino asignación continua y operador de concatenación
+module sum4_v2(output wire[3:0] S, output wire c_out, input wire[3:0] A, input wire[3:0] B, input wire c_in);
+
+endmodule
